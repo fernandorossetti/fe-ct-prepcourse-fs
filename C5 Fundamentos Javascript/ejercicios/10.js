0aadd2e8dc801f2c8f2f4b399e4c079d2f256673
fernado rossetti <fernando_r_4@hotmail.com>
2024-03-06 20:26:31 -0300

    New changes for homework Nº5

--- C5 Fundamentos Javascript/ejercicios/10.js
+++ C5 Fundamentos Javascript/ejercicios/10.js
@@ -1,8 +1,11 @@
 function esFechaValida(fecha) {
   // La función recibe un argumento "fecha".
-  // Comprueba si este corresponde a una fecha válida.
+  // Comprueba si este corresponde a una fecha válida.S
   // Si es así, retorna true, sino retorna false.
   // Tu código:
+  var dato = new Date(fecha);
+
+  return !isNaN(dato) && !isNaN(dato.getDate()) && dato.toString() !== 'Invalid Date';
 }
 
 module.exports = esFechaValida;
