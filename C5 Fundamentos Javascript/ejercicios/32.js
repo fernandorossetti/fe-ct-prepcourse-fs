474678a3df58458d17812a3d617e6fbe7f69ce56
Fernando Rossetti <fernando_r_4@hotmail.com>
2024-03-07 14:39:16 -0300

    Homework 5 finished

--- C5 Fundamentos Javascript/ejercicios/32.js
+++ C5 Fundamentos Javascript/ejercicios/32.js
@@ -2,6 +2,10 @@ function deEuroAdolar(euro) {
   // Debes calcular el valor recibido como argumento pasándolo a dólares.
   // Supongamos que 1 euro equivale a 1.20 dólares.
   // Tu código:
+  const dolar = 1.20;
+  const total = euro * dolar;
+
+  return total;
 }
 
 module.exports = deEuroAdolar;
