e7a313e77eb3338efbdae6b9d509f78ee8364fac
fernado rossetti <fernando_r_4@hotmail.com>
2024-03-11 20:10:42 -0300

    New Changes

--- C6 Bucles/ejercicios/21.js
+++ C6 Bucles/ejercicios/21.js
@@ -4,6 +4,9 @@ function esPotenciaDeDos(numero) {
   // Devuelve true si lo es, sino devuelve false.
   // PISTA: Utiliza un bucle while.
   // Tu código:
+  while(Number.isInteger(Math.log2(numero))){
+    return true;
+  } return false;
 }
 
 module.exports = esPotenciaDeDos;
--- C6 Bucles/ejercicios/22.js
+++ C6 Bucles/ejercicios/22.js
@@ -6,6 +6,22 @@ function combine(str1, str2, str3) {
    // combine("abc", "", "123") == "a1b2c3"
    // combine("abc", "12345", "") == "a1b2c345"
    // combine("abc", "12345", "67") == "a16b27c345"
+   let resultado = '';
+   const longitudMaxima = Math.max(str1.length, str2.length, str3.length);
+
+   for (let i = 0; i < longitudMaxima; i++) {
+       if (i < str1.length) {
+           resultado += str1[i];
+       }
+       if (i < str2.length) {
+           resultado += str2[i];
+       }
+       if (i < str3.length) {
+         resultado += str3[i];
+     }
+   }
+
+   return resultado;
 }
 
 module.exports = combine;
