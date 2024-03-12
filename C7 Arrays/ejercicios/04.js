d05f9854267fd966d9293794c0612db6b333d66c
Fernando Rossetti <fernando_r_4@hotmail.com>
2024-03-12 14:48:13 -0300

    New Changes

--- C7 Arrays/ejercicios/04.js
+++ C7 Arrays/ejercicios/04.js
@@ -2,6 +2,10 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Agrega el "elemento" al final del arreglo recibido.
   // Retorna el arreglo.
   // Tu código:
+ var array1 = [];
+ array = array1.push(elemento); 
+
+ return array;
 }
 
 module.exports = agregarItemAlFinalDelArray;
