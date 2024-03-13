c5e9abcde9b469bb69a0b0a3e5da247f5a6e5c9e
fernado rossetti <fernando_r_4@hotmail.com>
2024-03-12 23:04:34 -0300

    New Changes

--- C7 Arrays/ejercicios/09.js
+++ C7 Arrays/ejercicios/09.js
@@ -2,6 +2,9 @@ function obtenerElementoAleatorio(array) {
    // Devuelve un elemento aleatorio del arreglo array.
    // PISTA: Usa el método Math.random().
    // Tu código:
+   const elementAleatory = Math.floor(Math.random() * array.length);
+
+   return array[elementAleatory];
 }
 
 module.exports = obtenerElementoAleatorio;
