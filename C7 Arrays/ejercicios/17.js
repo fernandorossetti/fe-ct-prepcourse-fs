fd8b518ffa6c5aa298b6a9e2feb6db2f877e479e
Fernando Rossetti <fernando_r_4@hotmail.com>
2024-03-13 14:54:08 -0300

    New changes

--- C7 Arrays/ejercicios/17.js
+++ C7 Arrays/ejercicios/17.js
@@ -1,6 +1,12 @@
 function agregarNumeros(arrayOfNums) {
   // Suma todos los elementos de arrayOfNums y retorna el resultado.
   // Tu código:
+  var sumaArray = 0;
+
+  for(var i = 0;i < arrayOfNums.length; i++){
+    sumaArray += arrayOfNums[i];
+  }
+  return sumaArray;
 }
 
 module.exports = agregarNumeros;
--- C7 Arrays/ejercicios/18.js
+++ C7 Arrays/ejercicios/18.js
@@ -1,6 +1,16 @@
 function promedioResultadosTest(resultadosTest) {
   // Itera sobre los elementos del arreglo resultadosTest y devuelve el promedio de las notas.
   // Tu código:
+  const contador = 0;
+  const proArray = [];
+
+  for(var i = 0;i < resultadosTest.length; i++){
+    if(typeof resultadosTest[i] === "number"){
+      contador++;
+    }
+  }
+  proArray = resultadosTest[i] / contador;
+  return proArray;
 }
 
 module.exports = promedioResultadosTest;
