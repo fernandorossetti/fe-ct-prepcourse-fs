ddb93286269121735aefdafd2ea0a46b1f064e23
Fernando Rossetti <fernando_r_4@hotmail.com>
2024-04-05 12:06:05 -0300

    changes

--- C8 Objects + Callbacks/objects/ejercicios/06.js
+++ C8 Objects + Callbacks/objects/ejercicios/06.js
@@ -2,6 +2,14 @@ const contarPropiedades = (objeto) => {
   // Cuenta y retorna el total de propiedades que tiene el objeto.
   // PISTA: Puedes iterarlo usando el bucle for-in.
   // Tu código:
+  let totalPropiedades = 0;
+
+  for (let propiedad in objeto) {
+    if (objeto.hasOwnProperty(propiedad)) {
+      totalPropiedades++;
+    }
+  }
+  return totalPropiedades;
 };
 
 module.exports = contarPropiedades;
--- C8 Objects + Callbacks/objects/ejercicios/07.js
+++ C8 Objects + Callbacks/objects/ejercicios/07.js
@@ -7,6 +7,14 @@ function sort(sortBy, list) {
    // recibes --> ("a", [{ a: 1, b: 3 }, { a: 3, b: 2 }, { a: 2, b: 40 }])
    // retorna --> [{ a: 3, b: 2 }, { a: 2, b: 40 }, { a: 1, b: 3 }]
    // Tu código:
+   list.sort((a, b) =>{
+      if (a[sortBy] < b[sortBy]){
+         return 1;
+      } if (a[sortBy] > b[sortBy]){
+         return -1;
+      } else return 0;
+   });
+   return list;
 }
 
 module.exports = sort;
